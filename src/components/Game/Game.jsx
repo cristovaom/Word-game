605d9189e65c2e974395fe16362cfa6a1d380c21
Cristovão Martins <cristovaom8855@gmail.com>
2024-09-19 23:07:52 -0300

    finish

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,45 +12,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const [guess, setGuess] = React.useState([]);
-  const [disabledInputs, setDisabledInputs] = React.useState(false);
-  const[inputValue, setInputValue] = React.useState('');
+  const [guesses, setGuesses] = React.useState([]);
 
-  useEffect(() => {
-    console.log(guess)
-  },[guess])
-  function handleSubmit(event){
-    event.preventDefault();
-    if (!disabledInputs && inputValue.length === 5) {
-      setGuess([...guess, inputValue]); 
-      setInputValue(""); 
-    }
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
   }
+
   return (
     <>
-     <form class="guess-input-wrapper" onSubmit={handleSubmit}>
-
-        <div class="guess-results">
-        {guess && guess.map((letter, index) => (
-              <p key={index}>Your guess: {letter}</p>
-            ))}
-          
-        </div>
-      
-      <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" onChange={(event) => {
-            const input = event.target.value.toUpperCase();
-            if (input.length > 5) {
-              setDisabledInputs(true);
-            } else {
-              setDisabledInputs(false);
-              setInputValue(input);
-            }
-          }} style={{textTransform: "uppercase"}}/>
-      {disabledInputs && <span style={{color: 'red'}}>Minimo de 5 caracteres e maximo de 5 tambem</span>}
-    </form>
+      <GuessResults guesses={guesses} answer={answer} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
-  )
+  );
 }
 
 export default Game;
